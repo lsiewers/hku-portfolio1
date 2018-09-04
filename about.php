d70230e2c75e20048b2cc8bef05199e6fb2ca257
Luuk <luuk@siewers.org>
2018-09-04 19:06:22 +0200

    les 1 (04-09-'18)

---
+++ about.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Luuk Siewers' Portfolio</title>
+    <link href="styles.css" rel="stylesheet" type="text/css" media="screen">
+  </head>
+  <body>
+    <header>
+      <nav>
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="portfolio.php">Portfolio</a></li>
+          <li><a href="curriculum.php">CV</a></li>
+          <li><a href="about.php">Over mij</a></li>
+          <li><a href="contact.php">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  
+    <main>
+      <h1>Wie ben ik?</h1>
+      <p>
+        Ik ben een 20-jarige student Interaction Design aan de HKU. Mijn interesses liggen dan ook voornamelijk bij design, technologie en een stukje psychologie.
+        Naast studiegerelateerde interesses, ben ik een groot fan van muziek (genres blues, rock & soul) en boksen.
+      </p>
+
+      <article>
+        <h1>Wat is Interaction volgens mij?</h1>
+        <p></p>
+      </article>
+    </main>
+
+    <footer>
+      &copy; Luuk Siewers
+    </footer>
+  </body>
+</html>
---
+++ contact.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Luuk Siewers' Portfolio</title>
+    <link href="styles.css" rel="stylesheet" type="text/css" media="screen">
+  </head>
+  <body>
+    <header>
+      <nav>
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="portfolio.php">Portfolio</a></li>
+          <li><a href="curriculum.php">CV</a></li>
+          <li><a href="about.php">Over mij</a></li>
+          <li><a href="contact.php">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  
+    -- MAIN CONTENT --
+
+    <footer>
+      &copy; Luuk Siewers
+    </footer>
+  </body>
+</html>
